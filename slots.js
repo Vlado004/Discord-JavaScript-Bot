126b1bf63c96a3b8d08ec13957d03cc76441a5b7
Mateo Mamut <45850066+Vlado004@users.noreply.github.com>
2019-02-01 01:42:32 +0100

    Update slots.js

--- slots.js
+++ slots.js
@@ -1,28 +1,29 @@
+var functions = require("./functions.js");
 console.log("Slots - loaded.");
+module.exports = {
 
-bot.on('message', function(user, userID, channelID, message, event) {
-    var msg_split = message.split(" ");
-
-
-
-    if (msg_split[0].toLowerCase() == prefix + "slots") {
-
-
-        /**Checking if enough points**/
-        pool.getConnection(function(err, connection) {
+          /**Checking if enough points**/
+        main: function(userID, pool, bot, channelID) {
+          pool.getConnection(function(err, connection) {
             connection.query("select * from users where user_id = ? ", [userID], function(err, rows, fields) {
 
                 /**Enough points, we move on**/
-                if (parseInt(rows[0].points) >= 100) {
-                    functions.Update("update users set points = points-100 where user_id =  '" + userID + "' ", pool);
+                if (parseInt(rows[0].points) >= 250) {
+                    functions.Update("update users set points = points-250 where user_id =  '" + userID + "' ", pool);
 
 
                     var signPossible = [":spades:", ":spades:", ":spades:", ":spades:",
                         ":clubs:", ":clubs:", ":clubs:", ":clubs:",
                         ":hearts:", ":hearts:", ":hearts:", ":hearts:",
                         ":diamonds:", ":diamonds:", ":diamonds:", ":diamonds:",
-                        ":large_orange_diamond:", ":large_orange_diamond:",
-                        ":large_blue_diamond:", ":large_blue_diamond:",
+                        ":white_circle:", ":white_circle:", ":white_circle:", ":white_circle:",
+                        ":black_circle:", ":black_circle:", ":black_circle:", ":black_circle:",
+                        ":red_circle:", ":red_circle:", ":red_circle:", ":red_circle:",
+                        ":large_blue_circle:", ":large_blue_circle:", ":large_blue_circle:", ":large_blue_circle:",
+                        ":o:", ":o:",
+                        ":x:", ":x:",
+                        ":white_square_button:", ":white_square_button:",
+                        ":black_square_button:", ":black_square_button:",
                         ":100:"
                     ];
 
@@ -34,89 +35,51 @@ bot.on('message', function(user, userID, channelID, message, event) {
 
                     if (leftSign == middleSign && middleSign == rightSign) {
 
-                        if (middleSign == ":spades:") {
-                             functions.Update("update users set points = points + 250 where user_id = '" + userID + "' ", pool);
-                            outcome = "You got 250 points.";
-
+                        if (middleSign == ":spades:" || middleSign == ":clubs:" || middleSign == ":hearts:" || middleSign == ":diamonds:") {
+                             functions.Update("update users set points = points + 10000 where user_id = '" + userID + "' ", pool);
+                            outcome = "You got **10.000** points.";
                         }
 
-                        if (middleSign == ":clubs:") {
-                             functions.Update("update users set points = points + 250 where user_id = '" + userID + "' ", pool);
-                            outcome = "You got 250 points.";
-
+                        if (middleSign == ":white_circle:" || middleSign == ":black_circle:" || middleSign == ":red_circle:" || middleSign == ":large_blue_circle:") {
+                             functions.Update("update users set points = points + 10000 where user_id = '" + userID + "' ", pool);
+                            outcome = "You got **10.000** points.";
                         }
 
-                        if (middleSign == ":hearts:") {
-                             functions.Update("update users set points = points + 250 where user_id = '" + userID + "' ", pool);
-                            outcome = "You got 250 points.";
-
-                        }
-
-                        if (middleSign == ":diamonds:") {
-                             functions.Update("update users set points = points + 250 where user_id = '" + userID + "' ", pool);
-                            outcome = "You got 250 points.";
-
-                        }
-
-                        if (middleSign == ":large_orange_diamond:") {
-                             functions.Update("update users set points = points + 500 where user_id = '" + userID + "' ", pool);
-                            outcome = "You got 500 points.";
-
-                        }
-
-                        if (middleSign == ":large_blue_diamond:") {
-                             functions.Update("update users set points = points + 500 where user_id = '" + userID + "' ", pool);
-                            outcome = "You got 500 points.";
+                        if (middleSign == ":white_square_button:" || middleSign == ":x:" || middleSign == ":o:" || middleSign == ":black_square_button:") {
+                             functions.Update("update users set points = points + 100000 where user_id = '" + userID + "' ", pool);
+                            outcome = "You got **100.000** points.";
 
                         }
 
                         if (middleSign == ":100:") {
-                             functions.Update("update users set points = points + 1000 where user_id = '" + userID + "' ", pool);
-                            outcome = "You got 1000 points.";
+                             functions.Update("update users set points = points + 1000000 where user_id = '" + userID + "' ", pool);
+                            outcome = "**POG** You got **1.000.000** points!";
 
                         }
 
                     } else if ((middleSign == leftSign) || (middleSign == rightSign)) {
 
-                        if (middleSign == ":spades:") {
-                             functions.Update("update users set points = points + 50 where user_id = '" + userID + "' ", pool);
-                            outcome = "You got 50 points.";
+                        if (middleSign == ":spades:" || middleSign == ":clubs:" || middleSign == ":hearts:" || middleSign == ":diamonds:") {
+                             functions.Update("update users set points = points + 500 where user_id = '" + userID + "' ", pool);
+                            outcome = "You got **500** points.";
 
                         }
 
-                        if (middleSign == ":clubs:") {
-                             functions.Update("update users set points = points + 50 where user_id = '" + userID + "' ", pool);
-                            outcome = "You got 50 points.";
+                        if (middleSign == ":white_circle:" || middleSign == ":black_circle:" || middleSign == ":red_circle:" || middleSign == ":large_blue_circle:") {
+                             functions.Update("update users set points = points + 500 where user_id = '" + userID + "' ", pool);
+                            outcome = "You got **500** points.";
 
                         }
 
-                        if (middleSign == ":hearts:") {
-                             functions.Update("update users set points = points + 50 where user_id = '" + userID + "' ", pool);
-                            outcome = "You got 50 points.";
-
-                        }
-
-                        if (middleSign == ":diamonds:") {
-                             functions.Update("update users set points = points + 50 where user_id = '" + userID + "' ", pool);
-                            outcome = "You got 50 points.";
-
-                        }
-
-                        if (middleSign == ":large_orange_diamond:") {
-                             functions.Update("update users set points = points + 150 where user_id = '" + userID + "' ", pool);
-                            outcome = "You got 150 points.";
-
-                        }
-
-                        if (middleSign == ":large_blue_diamond:") {
-                             functions.Update("update users set points = points + 150 where user_id = '" + userID + "' ", pool);
-                            outcome = "You got 150 points.";
+                        if (middleSign == ":white_square_button:" || middleSign == ":x:" || middleSign == ":o:" || middleSign == ":black_square_button:") {
+                             functions.Update("update users set points = points + 5000 where user_id = '" + userID + "' ", pool);
+                            outcome = "You got **5.000** points.";
 
                         }
 
                         if (middleSign == ":100:") {
-                             functions.Update("update users set points = points + 250 where user_id = '" + userID + "' ", pool);
-                            outcome = "You got 250 points.";
+                             functions.Update("update users set points = points + 50000 where user_id = '" + userID + "' ", pool);
+                            outcome = "You got **50.000** points.";
 
                         }
 
@@ -142,6 +105,5 @@ bot.on('message', function(user, userID, channelID, message, event) {
             connection.release();
         });
 
+      }
     }
-
-});
